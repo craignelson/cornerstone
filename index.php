4379fb92009cf99faa567989e94ef98ecada4521
craignelson <craig@busybit.co.uk>
2020-01-24 13:55:03 +0000

    working files

---
+++ index.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Cornerstone Care</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<header>
+<div class="jumbotron text-center hero-image">
+    <div class="container">
+        <div class="row">
+        <div class="col-sm-1 logo"><img src="assets/logo.png" width="100px"></div>
+        <div class="col-sm-10 white-text"> Care for all the family from the number one pharmacy brand*</div>
+        <div class="col-sm-1"></div>
+        </div>
+    </div>
+    
+  <h1>Trusted by Millions of Families<br>across the UK for over 28 Years!</h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...<br>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p> 
+  
+  <button type="button" class="btn btn-light button-margin"> Find Out More </button>
+</div>
+</header>
+<div class="container">
+  <div class="row margin-100">
+    <div class="col-sm-6">
+      <h2>Over 80 Tried and tested remedies for minor ailments</h2>
+    </div>
+    <div class="col-sm-6">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+  </div>
+</div>
+    
+<div class="container text-center">
+  <div class="row">
+    <div class="col-sm-6 ">
+        <div class="survival-guide">
+        <h2 class="white-back">Summer essentials survival guide for Holidays</h2>
+        <p>Explore guide</p>
+        </div>      
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+          <button type="button" class="btn btn-primary "> Find Out More </button>
+    </div>
+
+    <div class="col-sm-6">
+        <div class="care-plus">
+        <h2 class="white-back">Summer essentials survival guide for Holidays</h2>
+        <p>Explore guide</p>
+        </div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+          <button type="button" class="btn btn-primary "> Find Out More </button>
+    </div>
+  </div>
+</div>
+    <div class="mid-headline">
+        <h2>Care for you and your family</h2>
+    </div>
+    
+<div class="container text-center">
+  <div class="row">
+    <div class="col-sm-3 ">
+        <div class="cough">
+            <img src="assets/prod_cat_07.png" height="220px">
+            <p class="cough-text under-image-text">Cough</p>
+        </div>      
+    </div>
+    
+    <div class="col-sm-3 ">
+        <div class="cold">
+            <img src="assets/prod_cat_16.png" height="220px">
+            <p class="cold-text under-image-text">Cold & Flu</p>
+        </div>      
+    </div>
+
+    <div class="col-sm-3">
+        <div class="pain">
+            <img src="assets/prod_cat_13.png" height="220px">
+            <p class="pain-text under-image-text">Pain Relief</p>
+        </div>
+    </div>
+      
+    <div class="col-sm-3 ">
+        <div class="skincare">
+            <img src="assets/prod_cat_10.png" height="220px">
+            <p class="skin-text under-image-text">Skincare</p>
+        </div>
+    </div>
+  </div>
+      <div class="row">
+    <div class="col-sm-3 ">
+        <div class="stomach">
+            <img src="assets/prod_cat_26.png" height="220px">
+            <p class="stomach-text under-image-text">Stomach & Bowel</p>
+        </div>      
+    </div>
+    
+    <div class="col-sm-3 ">
+        <div class="infant">
+            <img src="assets/prod_cat_29.png" height="220px">
+            <p class="infant-text under-image-text">Infant & Junior</p>
+        </div>      
+    </div>
+
+    <div class="col-sm-3">
+        <div class="female">
+            <img src="assets/prod_cat_32.png" height="220px">
+            <p class="female-text under-image-text">Female</p>
+        </div>
+    </div>
+      
+    <div class="col-sm-3 ">
+        <div class="other">
+            <img src="assets/prod_cat_23.png" height="220px">
+            <p class="other-text under-image-text">Other</p>
+        </div>
+    </div>
+  </div>
+</div>
+    <div class="mid-section"> </div>
+    <div class="overlay-container">
+        <div class="mid-section-overlay">
+            <h2>What our lovely customers have to say!
+            </h2>
+        </div>
+    </div>
+<footer>
+    <div class="container">
+        <div class="row">
+                <div class="col-sm-3 ">
+                    <h4><img src="assets/logo.png" width="100px"></h4>
+                    <p>text</p>
+                </div>
+                <div class="col-sm-3 ">
+                    <h4>Our Ranges</h4>
+                    <p>Links</p>
+                    <p>Links</p>
+                    <p>Links</p>
+                    <p>Links</p>
+                    
+                </div>
+                <div class="col-sm-3 ">
+                    <h4>A little care from us</h4>
+
+                    <p>Links</p>
+                    <p>Links</p>
+                    <p>Links</p>
+                </div>
+                <div class="col-sm-3 ">
+                    <h4>Stay in touch</h4>
+                    <p>Social Icons</p>
+                    <p>Links</p>
+                    <p>Links</p>
+                    <p>Links</p>
+                    <p>Links</p>
+                </div>
+            
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+                <div class="col-sm-6 ">
+                    <p class="small-footer-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+                </div>
+                <div class="col-sm-6 ">
+                    <p class="small-footer-text">Lorem | ipsum | dolor | sit | amet | <a href="/">Link</a> </p>
+                </div>
+            
+        </div>
+    </div> 
+    </footer>
+</body>
+</html>
